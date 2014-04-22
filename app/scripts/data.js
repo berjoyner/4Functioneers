0b599c57b139cdaed341b355d4ca3ed944d4cc40
Devon Hicks <devon@devonhicks.com>
2014-04-22 15:32:59 -0400

    templates for about page

---
+++ app/scripts/data.js
@@ -0,0 +1,43 @@
+var teamBios = [ 
+   {
+		
+	"functioneer" : {
+		"name": "Amber",
+		"bio": "I was born an avatar, then something happened, now I'm a nerd girl.",
+		"twitter": "hi"
+
+		}
+	},
+	{
+
+	"functioneer": {
+		"name": "Brian",
+		"bio": "I was born a baby. I grew up in Ohio. I am addicted to Dr. Pepper and Chipotle.",
+		"twitter": "/holycitygeek"
+		}
+
+	},	
+
+	{
+
+	"functioneer": {
+		"name": "Devon",
+		"bio": "I was born a chocolate chip, and grew up in Knoxville. I'm addicted to cookies.",
+		"twitter": "hi"
+		
+		}
+
+	},
+
+	{
+
+	"functioneer":{
+		"name": "Mike",
+		"bio": "I'm pretty sure I was born in a Flannery O'Connor short story. I've been an electrician, a teacher, and a video guy. Now I am trying to be a front end developer.",
+		"twitter": "/guidestonesfilm"
+		
+		}
+
+	}	
+
+];
