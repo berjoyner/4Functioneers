0b745283f1ced2db16cb90121fdb38f874244be6
Brian Buchanan <brianbuchanan@Brians-MacBook-Air.local>
2014-04-21 21:39:33 -0400

    transportation.js updates

--- app/scripts/transportation.js
+++ app/scripts/transportation.js
@@ -6,3 +6,14 @@ function Transportation(type, budget) {
 var Limo = new Transportation("Limo", 100);
 var Taxi = new Transportation("Taxi", 50);
 var Car = new Transportation("Car", 25);
+
+if (this.budget === 25) {
+ $( ".transportation" ).append( "<h1>You will be taking a car!</h1>" )
+ 	console.log("Car!!!!!");
+}
+if (this.budget === 50) {
+ $( ".transportation" ).append( "<h1>You will be taking a Taxi!</h1>" )
+}
+if (this.budget === 100) {
+ $( ".transportation" ).append( "<h1>You will be taking a Limo!</h1>" )
+}}
