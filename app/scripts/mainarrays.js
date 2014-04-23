c02065615b00f1564015945075876bfbcf996dd9
Berjoyner <joyner.amber@gmail.com>
2014-04-23 14:54:19 -0400

    Arrays All In One Place

---
+++ app/scripts/mainarrays.js
@@ -0,0 +1,54 @@
+var obj = {
+
+			transportation: {
+				twentyfive: [{text: "You will be taking a Rickshaw", picUrl: "images/transportation/rickshaw25.jpg"},
+							{text: "You will be taking your car and parking downtown", picUrl: "images/transportation/car25.jpg"}],
+				fifty: 		[{text: "You will be taking a Taxi", picUrl: "images/transportation/taxi50.jpg"},
+							{text: "You will be taking a Water Taxi", picUrl: "images/transportation/watertaxi50.jpg"}],
+				hundred: 	[{text: "You will be taking a Limo", picUrl: "images/transportation/limo100.jpg"},
+							{text: "You will be taking an Exotic Rental Car", picUrl: "images/transportation/exotic100.jpg"}],
+			},
+
+			food: 			{
+				twentyfive: [{text: "Your budget allows for dinner at Shelter!", picUrl: "images/restaurants/shelter.jpg"},
+							 {text: "Your budget allows for dinner at Sesame!", picUrl: "images/restaurants/sesame.gif"},
+							 {text: "Your budget allows for dinner at Tasty Thai!", picUrl: "images/restaurants/teasty.png"},	
+							 {text: "Your budget allows for dinner at Noisy Oyster!", picUrl: "images/restaurants/noisy.jpg"},
+							 {text: "Your budget allows for dinner at Reds!", picUrl: "images/restaurants/reds.png"},
+							]
+				fifty: 		[{text: "Your budget allows for dinner at Coleman Public House!", picUrl: "images/restaurants/colemanpublichouse.png"},
+							 {text: "Your budget allows for dinner at EVO!", picUrl: "images/restaurants/evo.png"},
+							 {text: "Your budget allows for dinner at Tsunami!", picUrl: "images/restaurants/tsunami.png"},
+							 {text: "Your budget allows for dinner at Bacco!", picUrl: "images/restaurants/bacco.png"},
+							 {text: "Your budget allows for dinner at Hymans!", picUrl: "images/restaurants/hymans.jpg"},	
+							]
+				onehundred:[ {text: "Your budget allows for dinner at Halls Chophouse!", picUrl: "images/restaurants/halls100.jpg"},
+							 {text: "Your budget allows for dinner at Oak Steak House!", picUrl: "images/restaurants/oak100.jpg"},
+							 {text: "Your budget allows for dinner at Basil!", picUrl: "images/restaurants/basil100.jpg"},
+							 {text: "Your budget allows for dinner at Hank's Seafood!", picUrl: "images/restaurants/hanks100.jpg"},
+							 {text: "Your budget allows for dinner at Fulton Five!", picUrl: "images/restaurants/fultonfive100.jpg"},
+							 ]
+			},
+
+			activity: {
+				twentyfive: [{text: "Your budget allows for a Movie!", picUrl: "images/activities/movies.jpg"}, 
+							 {text: "Your budegt allows for a Mini-Golf Session!", picUrl: "images/activities/mini-golf.jpg"},
+							 {text: "Your budget allows for a Ghost Tour!", picUrl: "images/activities/ghost.jpg"},
+							 {text: "Your budget allows for a Fort Sumter Tour!", picUrl: "images/activities/ftsumter.jpg"},
+							 {text: "Your budget allows for a Trip to the Aquarium!", picUrl: "images/activities/aquarium.jpeg"}
+							 ]
+				fifty:  	[{text: "Your budget allows for a Kayaking Session!", picUrl: "images/activities/kayak.png"},
+							 {text: "Your budget allows for a Painting Session!", picUrl: "images/activities/fearnoeasel.png"},
+							 {text: "Your budget allows for a Go-Kart Session!", picUrl: "images/activities/gokart.gif"},
+							 {text: "Your budget allows for a Laser-Tag Session!", picUrl: "images/activities/lasertag.png"},
+							 {text: "Your budget allows for a PaintBall Session!", picUrl: "images/activities/paintball.jpg"}
+							]
+				hundred: 	[{text: "Your budget allows for a Private Boat Charter!", picUrl:"images/activities/boatcharter100.jpg"},
+							 {text: "Your budget allows for a Private Carriage Ride!", picUrl: "images/activities/carriage100.jpg"},
+							 {text: "Your budget allows for a Concert!", picUrl: "images/activities/concert100.jpg"},
+							 {text: "Your budget allows for a Day at Magnolia Plantation!", picUrl: "images/activities/magnolia100.jpg"},
+							 {text: "Your budget allows for a Private Jump Session at SkyZone!", picUrl: "images/activities/skyzone100.jpg"}
+							 ]
+			}
+
+}
