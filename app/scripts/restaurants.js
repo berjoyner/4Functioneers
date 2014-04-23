41ae124e8b7a7974a35bdc93dfb23ffe02444db0
MikeESL <mike.reser.03@gmail.com>
2014-04-23 14:43:48 -0400

    committing food arrays

--- app/scripts/restaurants.js
+++ app/scripts/restaurants.js
@@ -1,126 +1,20 @@
-function Restaurant (type, budget){
-		this.type = type;
-		this.budget = budget;
-
-		this.restaurantSelect = function () {
-			if (this.type === "American"){
-				if (this.budget === 25){
-					var chance = Math.floor(Math.random() * 10 )
-					if (chance <= 5){
-					$(".restaurant").append("<h1>You will eat at Shelter tonight!</h1>");	
-					console.log("Shelter");
-						}else{
-							$(".restaurant").append("<h1>You will eat at Seasame tonight!</h1>");
-							console.log ("Seasame");
-}
-}
-					if (this.budget === 50){
-					var chance = Math.floor(Math.random() * 10 )
-					if (chance <= 5){
-					$(".restaurant").append("<h1>You will eat at Coleman Public House tonight!</h1>");	
-					console.log("CPH")
-						}else{
-							$(".restaurant").append("<h1>You will eat at EVO tonight!</h1>")
-							console.log ("EVO")
-}
-}
-					if (this.budget === 100){
-					var chance = Math.floor(Math.random() * 10 )
-					if (chance <= 5){
-					$(".restaurant").append("<h1>You will eat at Halls Chophouse tonight!>/h1>")
-					console.log === ("Halls")
-						}else{
-							$(".restaurant").append("<h1>You will eat at Oak Steak House tonight!</h1>")
-							console.log("Oak")
-}
-}
-}
-			if (this.type === "Asian"){
-				if (this.budget === 25){
-					var chance = Math.floor(Math.random() * 10 )
-					if (chance <= 5){
-					$(".restaurant").append("<h1>You will eat at Tasty Thai tonight!</h1>");	
-					console.log("Tasty");
-						}else{
-							$(".restaurant").append("<h1>You will eat at Co tonight!</h1>");
-							console.log ("Co");
-}
-}
-				if (this.budget === 50){
-					var chance = Math.floor(Math.random() * 10 )
-					if (chance <= 5){
-					$(".restaurant").append("<h1>You will eat at Tsunami tonight!</h1>");	
-					console.log("Tsunami");
-						}else{
-							$(".restaurant").append("<h1>You will eat at Wasabi tonight!</h1>");
-							console.log ("Wasabi");
-}
-}
-				if (this.budget === 100){
-					var chance = Math.floor(Math.random() * 10 )
-					if (chance <= 5){
-					$(".restaurant").append("<h1>You will eat at Basil tonight!</h1>");	
-					console.log("Basil");
-						}else{
-							$(".restaurant").append("<h1>You will eat at PF Chang's tonight!</h1>");
-							console.log ("PFC");
-}
-}
-}
-			if (this.type === "Seafood"){
-				if (this.budget === 25){
-					var chance = Math.floor(Math.random() * 10 )
-					if (chance <= 5){
-					$(".restaurant").append("<h1>You will eat at Red's tonight!</h1>");	
-					console.log("Reds");
-						}else{
-							$(".restaurant").append("<h1>You will eat at Noisy Oyster tonight!</h1>");
-							console.log ("Oyster");
-}
-}
-				if (this.budget === 50){
-					var chance = Math.floor(Math.random() * 10 )
-					if (chance <= 5){
-					$(".restaurant").append("<h1>You will eat at Fleet's Landing tonight!</h1>");	
-					console.log("Fleets");
-						}else{
-							$(".restaurant").append("<h1>You will eat at Hyman's tonight!</h1>");
-							console.log ("Hyman");
-}
-}
-				if (this.budget === 100){
-					var chance = Math.floor(Math.random() * 10 )
-					if (chance <= 5){
-					$(".restaurant").append("<h1>You will eat at Hank's tonight!</h1>");	
-					console.log("Hank");
-						}else{
-							$(".restaurant").append("<h1>You will eat at Red Drum tonight!</h1>");
-							console.log("Drum");
-}
-}
-}
-};
-var Shelter = new Restaurant ("American", 25);
-var Seasame = new Restaurant ("American", 25);
-var ColemanPublicHouse = new Restaurant ("American", 50);
-var Evo = new Restaurant ("American", 50);
-var Halls = new Restaurant ("American", 100);
-var Oak = new Restaurant ("American", 100);
-var Tasty = new Restaurant ("Asian", 25);
-var Co = new Restaurant ("Asian", 25);
-var Tsunami = new Restaurant ("Asian", 50);
-var Wasabi = new Restaurant ("Asian", 50);
-var Basil = new Restaurant ("Asian", 100);
-var PFChang = new Restaurant ("Asian", 100);
-var Reds = new Restaurant ("Seafood", 25);
-var NoisyOyster = new Restaurant ("Seafood", 25);
-var Fleets = new Restaurant ("Seafood", 50);
-var Hymans = new Restaurant ("Seafood", 50);
-var Hanks = new Restaurant ("Seafood", 100);
-var RedDrum = new Restaurant ("Seafood", 100);
-					
-				
-
-
-
- 
+food: {
+				twentyfive: [{text: "Your budget allows for dinner at Shelter!", picUrl: "images/restaurants/shelter.jpg"},
+							 {text: "Your budget allows for dinner at Sesame!", picUrl: "images/restaurants/sesame.gif"},
+							 {text: "Your budget allows for dinner at Tasty Thai!", picUrl: "images/restaurants/teasty.png"},	
+							 {text: "Your budget allows for dinner at Noisy Oyster!", picUrl: "images/restaurants/noisy.jpg"},
+							 {text: "Your budget allows for dinner at Reds!", picUrl: "images/restaurants/reds.png"},
+							]
+				fifty: 		[{text: "Your budget allows for dinner at Coleman Public House!", picUrl: "images/restaurants/colemanpublichouse.png"},
+							 {text: "Your budget allows for dinner at EVO!", picUrl: "images/restaurants/evo.png"},
+							 {text: "Your budget allows for dinner at Tsunami!", picUrl: "images/restaurants/tsunami.png"},
+							 {text: "Your budget allows for dinner at Bacco!", picUrl: "images/restaurants/bacco.png"},
+							 {text: "Your budget allows for dinner at Hymans!", picUrl: "images/restaurants/hymans.jpg"},	
+							]
+				onehundred:[ {text: "Your budget allows for dinner at Halls Chophouse!", picUrl: "images/restaurants/halls100.jpg"},
+							 {text: "Your budget allows for dinner at Oak Steak House!", picUrl: "images/restaurants/oak100.jpg"},
+							 {text: "Your budget allows for dinner at Basil!", picUrl: "images/restaurants/basil100.jpg"},
+							 {text: "Your budget allows for dinner at Hank's Seafood!", picUrl: "images/restaurants/hanks100.jpg"},
+							 {text: "Your budget allows for dinner at Fulton Five!", picUrl: "images/restaurants/fultonfive100.jpg"},
+							 ]
+			},
