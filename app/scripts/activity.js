5f0965baa1a13cf3a7fa19d66175db83bd730231
Berjoyner <joyner.amber@gmail.com>
2014-04-23 14:50:09 -0400

    Activity Array

--- app/scripts/activity.js
+++ app/scripts/activity.js
@@ -1,40 +1,24 @@
-function Activity(budget, indoor, outdoor) {
-	this.budget = budget;
-	this.indoor = budget;
-	this.outdoor = budget; 
+activity: {
+	twentyfive: [{text: "Your budget allows for a Movie!", picUrl: "images/activities/movies.jpg"}, 
+				 {text: "Your budegt allows for a Mini-Golf Session!", picUrl: "images/activities/mini-golf.jpg"},
+				 {text: "Your budget allows for a Ghost Tour!", picUrl: "images/activities/ghost.jpg"},
+				 {text: "Your budget allows for a Fort Sumter Tour!", picUrl: "images/activities/ftsumter.jpg"},
+				 {text: "Your budget allows for a Trip to the Aquarium!", picUrl: "images/activities/aquarium.jpeg"}
+				 ]
+	fifty:  	[{text: "Your budget allows for a Kayaking Session!", picUrl: "images/activities/kayak.png"},
+				 {text: "Your budget allows for a Painting Session!", picUrl: "images/activities/fearnoeasel.png"},
+				 {text: "Your budget allows for a Go-Kart Session!", picUrl: "images/activities/gokart.gif"},
+				 {text: "Your budget allows for a Laser-Tag Session!", picUrl: "images/activities/lasertag.png"},
+				 {text: "Your budget allows for a PaintBall Session!", picUrl: "images/activities/paintball.jpg"}
+				]
+	hundred: 	[{text: "Your budget allows for a Private Boat Charter!", picUrl:"images/activities/boatcharter100.jpg"},
+				 {text: "Your budget allows for a Private Carriage Ride!", picUrl: "images/activities/carriage100.jpg"},
+				 {text: "Your budget allows for a Concert!", picUrl: "images/activities/concert100.jpg"},
+				 {text: "Your budget allows for a Day at Magnolia Plantation!", picUrl: "images/activities/magnolia100.jpg"},
+				 {text: "Your budget allows for a Private Jump Session at SkyZone!", picUrl: "images/activities/skyzone100.jpg"}
+				 ]
+}
 
-	this.budAct = function(target) {
-		if (this.budget === 25) {
-
-			if(this.indoor === true) {
-			console.log("You are eligible for a Movie Activity")
-
-		} else {
-			console.log("You are eligible for a MiniGolf Activity")
-		}
-		
-		} else if(this.budget ====  50) {
-
-			if(this.indoor ==== true) {
-				console.log("You are eligible for a Painting Activity")
-
-		} else {
-				console.log("You are eligible for a Kayak Activity")
-
-		}
-
-		} else if(this.budget === 100) {
-
-			if(this.indoor === true) {
-				console.log("You are eligible for a Concert Activity")
-		}
-
-		} else {
-			console.log("You are eligible for a Fishing Charter Activity")
-		}
-
-
-	}
 
 
 
